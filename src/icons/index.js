121ea6c7fffa5ace9b978a536666c9fb4e955f20
guifeng <gzheng@hotmail.com>
2020-11-30 12:49:48 +0800

    add svg-icon

---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import SvgIcon from '@/components/SvgIcon'; // svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon);
+
+// 引入SVG文件
+const req = require.context('./svg', false, /\.svg$/);
+const requireAll = requireContext => requireContext.keys().map(requireContext);
+requireAll(req);
